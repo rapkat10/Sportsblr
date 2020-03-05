2320151f6c3378a6375fbea36fec857c733e23d3
rapkat10 <aminrapkat@gmail.com>
2020-03-05 03:47:17 -0700

    started posts backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,10 @@ class User < ApplicationRecord
         foreign_key: :user_id,
         class_name: :Post
 
+    has_many :posts,
+        foreign_key: :user_id,
+        class_name: :Post
+
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
         return nil unless user && user.is_password?(password)
