e8e3cfbce82791ad677c89f5f14c9a2a13cc25e1
rapkat10 <aminrapkat@gmail.com>
2020-03-09 03:59:02 -0600

    adding edit and delete

---
+++ frontend/components/posts/posts_forms/edit_text_posts_form_container.js
@@ -0,0 +1,32 @@
+import { connect } from 'react-redux';
+import EditTextPostsForm from './edit_text_posts_form';
+import { updatePost, getPosts } from '../../../actions/post_actions';
+import { closeModal } from '../../../actions/modal_actions';
+
+const mapStateToProps = (state, ownProps) => {
+    const formType = ownProps.formType;
+    const post = ownProps.post;
+    // debugger;
+    const currentUser = state.entities.users[state.session.id];
+    return ({
+        post: {
+            title: post.title,
+            body: post.body,
+            post_type: formType,
+            user_id: currentUser.id,
+            id: post.id
+        },
+        formType: formType,
+        currentUser: currentUser
+    });
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return ({
+        action: (post) => dispatch(updatePost(post)),
+        getPosts: () => dispatch(getPosts()),
+        closeModal: () => dispatch(closeModal())
+    });
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditTextPostsForm)
