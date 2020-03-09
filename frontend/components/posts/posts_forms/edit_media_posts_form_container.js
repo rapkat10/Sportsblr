f8958dda38dd1af161ae8c4386710dc4f07b820a
rapkat10 <aminrapkat@gmail.com>
2020-03-09 03:58:24 -0600

    adding edit and delete

---
+++ frontend/components/posts/posts_forms/edit_media_posts_form_container.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { editMediaPost } from '../../../actions/post_actions';import { closeModal } from '../../../actions/modal_actions';
+import EditMediaPostsForm from './edit_media_posts_form'
+
+const mapStateToProps = (state, ownProps) => {
+  const formType = ownProps.formType;
+  const post = ownProps.post;
+  const currentUser = state.entities.users[state.session.id];
+  return ({
+      post: {
+        title: post.title,
+        body: post.body,
+        post_type: formType,
+        user_id: currentUser.id,
+        id: post.id
+      },
+        img_url: post.img_url,
+        audio_url: post.img_url,
+        video_url: post.img_url,
+      formType: formType,
+      currentUser: currentUser
+  });
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    action: (post, postId) => dispatch(editMediaPost(post, postId)),
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditMediaPostsForm);
